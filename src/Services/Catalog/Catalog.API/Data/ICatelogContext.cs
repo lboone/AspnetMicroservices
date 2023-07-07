100fe3d8b2ba753610ea7db757372dd1509d3f6a
Lloyd Boone <lboone@progressive.com>
2023-07-06 23:19:27 -0400

    Added Data & Entity Layer to Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Data/ICatelogContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public interface ICatelogContext
+{
+    IMongoCollection<Product> Products { get; }
+}
+
